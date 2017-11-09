9b61e1445b42743e2abf760b55708dfd211c9f5d
Wen <alejet@Wens-MacBook-Pro-3.local>
2017-11-09 13:56:28 +0800

    admin/users/index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,11 @@
-class Admin::UsersController < ApplicationController
+class Admin::UsersController < Admin::AdminController
   #before_action :authenticate_user!
 
   def index
+    @users = User.all
+  end
+
+  def new
+    User.new
   end
 end
